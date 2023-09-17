68c2ce64bc1b3a7281531abfc8931292918540fc
bryansine <bryansine1738@gmail.com>
2023-09-17 08:07:32 -0700

    update

--- env_builtin.c
+++ env_builtin.c
@@ -1,19 +1,23 @@
 #include "shell.h"
 
-int env_builtin(__attribute((unused)) char **tokens_arr) {
-    int env_index = 0;
+int env_builtin(__attribute((unused)) char **tokens_arr)
+{
+	int env_index = 0;
 
-    while (__environ[env_index]) {
-        if (write(STDOUT_FILENO, __environ[env_index], strlen(__environ[env_index])) == -1) {
-            return (1);
-        }
-        if (write(STDOUT_FILENO, "\n", strlen("\n")) == -1) {
-            return (1);
-        }
-        env_index++;
-    }
+while (__environ[env_index])
+{
+	if (write(STDOUT_FILENO, __environ[env_index], strlen(__environ[env_index])) == -1)
+	{
+	return (1);
+	}
+if (write(STDOUT_FILENO, "\n", strlen("\n")) == -1)
+	{
+	return (1);
+	}
+env_index++;
+}
 
-    return (0);
+	return (0);
 }
 
 
