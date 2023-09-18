1e338c4f42bcc7ffb07fac8013100e1650f20cc0
bryansine <bryansine1738@gmail.com>
2023-09-18 06:54:02 -0700

    cd_builtin.c

--- cd_builtin.c
+++ cd_builtin.c
@@ -6,19 +6,20 @@ char *home_dir = getenv("HOME");
 
 	if (tokens_arr[1] == NULL)
 	{
-		if(home_dir == NULL) {
-            perror("Home directory not set");
-            return (1);
-        }
+		if (home_dir == NULL)
+		{
+		perror("Home directory not set");
+return (1);
+		}
 
-        if(chdir(home_dir) != 0)
-	{
-	return (2);
-        }
+	if (chdir(home_dir) != 0)
+		{
+		return (2);
+		}
 	}
 	else
 	{
-		if (chdir(tokens_arr[1]) != 0)
+	if (chdir(tokens_arr[1]) != 0)
 		{
 	return (2);
 		}
