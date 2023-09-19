39349d2328d37f1a447b8bf8f60244a4aac9e491
Bradley Mwangangi <39657175+bradl33@users.noreply.github.com>
2023-09-19 16:20:52 +0300

    errors.c add description, remove whitespace

--- errors.c
+++ errors.c
@@ -1,25 +1,33 @@
 #include "shell.h"
 
-int err_not_found(char *program_name, size_t command_num, char *command_name) {
-    char *error_string = NULL;
-    char *str_command_num = _itos(command_num);
-    int err_str_size = (_strlen(program_name) + (2 * _strlen(": ")) +
-                    _strlen(str_command_num) + _strlen(command_name) +
-                    _strlen(": not found\n") + 1);
-    error_string = (char *) malloc(sizeof(char) * err_str_size);
+/**
+ * err_not_found - template for error not found
+ * @program_name: pointer to name of program
+ * @command_num: command number
+ * @command_name: command name
+ *
+ * Return: return 127
+*/
+int err_not_found(char *program_name, size_t command_num, char *command_name)
+{
+	char *error_string = NULL;
+	char *str_command_num = _itos(command_num);
+	int err_str_size = (_strlen(program_name) + (2 * _strlen(": ")) +
+					_strlen(str_command_num) + _strlen(command_name) +
+					_strlen(": not found\n") + 1);
+	error_string = (char *) malloc(sizeof(char) * err_str_size);
 
-    _strcpy(error_string, program_name);
-    _strcat(error_string, ": ");
-    _strcat(error_string, str_command_num);
-    _strcat(error_string, ": ");
-    _strcat(error_string, command_name);
-    _strcat(error_string, ": not found\n");
-    _strcat(error_string, "\0");
+	_strcpy(error_string, program_name);
+	_strcat(error_string, ": ");
+	_strcat(error_string, str_command_num);
+	_strcat(error_string, ": ");
+	_strcat(error_string, command_name);
+	_strcat(error_string, ": not found\n");
+	_strcat(error_string, "\0");
 
-    write(STDERR_FILENO, error_string, _strlen(error_string));
-    free(error_string);
-    free(str_command_num);
+	write(STDERR_FILENO, error_string, _strlen(error_string));
+	free(error_string);
+	free(str_command_num);
 
-    return(127);
+	return (127);
 }
-
