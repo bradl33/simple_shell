8cd64abab3c8557adbc46a8e9ee33f5b24d2aef3
Bradley Mwangangi <39657175+bradl33@users.noreply.github.com>
2023-09-19 21:41:41 +0300

    add description, remove whitespace

--- custom_str_funcs.c
+++ custom_str_funcs.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * _strcat - performs string concatenation
+ * @dest: char pointer to destination string
+ * @src: char pointer to source string
+ *
+ * Return: destination
+*/
 char *_strcat(char *dest, char *src)
 {
 	int i, x;
@@ -21,6 +28,14 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
+
+/**
+ * _strcpy - performs string copying
+ * @dest: char pointer to destination string
+ * @src: char pointer to source string
+ *
+ * Return: destination
+*/
 char *_strcpy(char *dest, char *src)
 {
 	int i, x;
@@ -38,6 +53,12 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+/**
+ * _strdup - performs string duplication
+ * @str: char pointer to string to be duplicated
+ *
+ * Return: duplicated string
+*/
 char *_strdup(char *str)
 {
 	char *dup_str = NULL;
@@ -62,6 +83,12 @@ char *_strdup(char *str)
 	return (dup_str);
 }
 
+/**
+ * _strlen - calculates length of string
+ * @str: char pointer to string to be evaluated
+ *
+ * Return: length of string
+*/
 int _strlen(char *str)
 {
 	int i = 0;
@@ -72,17 +99,24 @@ int _strlen(char *str)
 	return (i);
 }
 
+/**
+ * _itos - performs integer to string convertion
+ * @command_num: command number to be converted
+ *
+ * Return: string representation of command_num
+*/
 char *_itos(size_t command_num)
 {
 	size_t num = command_num;
 	int digits_in_num = 0;
 	char *string = NULL;
+
 	do {
 		num /= 10;
 		digits_in_num++;
 	} while (num > 0);
 
-	string = (char*)malloc(digits_in_num + 1);
+	string = (char *)malloc(digits_in_num + 1);
 
 	if (string == NULL)
 		return (NULL);
