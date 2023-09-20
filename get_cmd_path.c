7dbe2db7ed9f15288985ede745c6fa3577ac9a7d
bryansine <bryansine1738@gmail.com>
2023-09-20 04:20:54 -0700

    get_cmd_path.c add descriptions remove whitespacesZ

--- get_cmd_path.c
+++ get_cmd_path.c
@@ -1,48 +1,63 @@
 #include "shell.h"
 #include <sys/stat.h>
 
-char *get_cmd_path(char *command) {
-    char *path = getenv("PATH");
-    char *path_copy, *path_token, *file_path;
-    int cmd_length, dir_length;
-    struct stat buffer;
+/**
+ * get_cmd_path - Receives full path of a command executable.
+ * @commmand: command name for which the path is to be determined.
+ *
+ * Return: returns NULL if unsuccessfull.
+ */
 
-    path_copy = path_token = file_path = NULL;
+char *get_cmd_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *path_copy, *path_token, *file_path;
+	int cmd_length, dir_length;
+	struct stat buffer;
 
-    if (path != NULL) {
-        path_copy = strdup(path);
-        if (path_copy == NULL) {
-            return (NULL);
-        }
+	path_copy = path_token = file_path = NULL;
 
-        path_token = strtok(path_copy, ":");
+	if (path != NULL)
+	{
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+		{
+	return (NULL);
+		}
 
-        while (path_token != NULL) {
-            cmd_length = strlen(command);
-            dir_length = strlen(path_token);
-            file_path = malloc(dir_length + cmd_length + 2);
+	path_token = strtok(path_copy, ":");
 
-            if (file_path == NULL) {
-                free(path_copy);
-                return (NULL);
-            }
+	while (path_token != NULL)
+		{
+	cmd_length = strlen(command);
+	dir_length = strlen(path_token);
+	file_path = malloc(dir_length + cmd_length + 2);
 
-            strcpy(file_path, path_token);
-            strcat(file_path, "/");
-            strcat(file_path, command);
-            file_path[dir_length + cmd_length + 1] = '\0';
+	if (file_path == NULL)
+		{
+	free(path_copy);
+	return (NULL);
+		}
 
-            if (stat(file_path, &buffer) == 0) {
-                free(path_copy);
-                return (file_path);
-            } else {
-                free(file_path);
-                path_token = strtok(NULL, ":");
-            }
-        }
+	strcpy(file_path, path_token);
+	strcat(file_path, "/");
+	strcat(file_path, command);
+	file_path[dir_length + cmd_length + 1] = '\0';
 
-        free(path_copy);
-    }
+	if (stat(file_path, &buffer) == 0)
+	{
+		free(path_copy);
+		return (file_path);
+	}
+	else
+	{
+		free(file_path);
+		path_token = strtok(NULL, ":");
+	}
+		}
 
-    return (NULL);
+	free(path_copy);
+	}
+
+	return (NULL);
 }
